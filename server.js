e6147cd5c9769e0f0c89a59599045f1677b146cf
Mahakchhipa <mehekjajpuraom700@gmail.com>
2025-06-30 09:03:32 +0530

    Add files folder

---
+++ server.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+const express = require("express")
+const app = express();
+require('dotenv').config()
+const port = process.env.port
+
+const mongourl = process.env.MOONGO_URL
+
+mongoose.connect.MOONGO_URL
+
+app.use(express.json())
+
+// const superadminroute = require('./router/SuperAdminRoute');
+// const clientroute = require('./router/clientRoute');
+// const userroute = require('./router/userRoute');
+// app.use('/superadmins', superadminroute);
+// app.use('/clients', clientroute);
+// app.use('/users', userroute);
+
+
+app.listen(port , ()=>{
+    console.log( ` Server is ruuning on this port ${port}`)
+})
